9819cd77b066f93c765da9b5c6d09c2b86f86da2
AnotherTwinkle <anothersparklingstar@gmail.com>
2021-07-24 16:59:44 +0600

    Discord timestamp utils

--- cogs/utils/utils.py
+++ cogs/utils/utils.py
@@ -1,22 +1,30 @@
 from io import BytesIO
-
-import discord
+import time
 import aiohttp
+
+from discord.ext import commands
+import discord
 
 
 async def file_from_url(url: str, filename: str = 'file.png', session : aiohttp.ClientSession = None):
+	asession = session if session else aiohttp.ClientSession() #Create a new session if not provided
 
-    asession = session if session else aiohttp.ClientSession() #Create a new session if not provided
+	async with asession.get(url) as resp:
+		data = await resp.content.read()
 
-    async with asession.get(url) as resp:
-        data = await resp.content.read()
+	file = discord.File(fp= BytesIO(data), filename= filename)
+	if not session: # Sesssion was not provided, so we will close our created session
+		await asession.close()
 
-    file = discord.File(fp= BytesIO(data), filename= filename)
+	return file
+	
 
-    if not session: # Sesssion was not provided, so we will close our created session
-        await asession.close()
+def discord_timestamp(target: int= time.time(), style : str = 'f'):
+	valid = ['t','T','d','D','f','F','R']
+	#https://github.com/discord/discord-api-docs/blob/ff4d9d8ea6493405a8823492338880c47fb02996/docs/Reference.md#timestamp-styles
+	if style not in valid:
+		raise commands.BadArgument('Invalid timestamp style')
 
-    return file
-    
+	return f'<t:{target}:{style}>'
+	
 
-def discord_timestamp
