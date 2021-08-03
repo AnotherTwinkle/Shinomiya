95b8992571407cd30dd7c413ddc167b09629b8a5
AnotherTwinkle <anothersparklingstar@gmail.com>
2021-08-04 00:35:06 +0600

    Support for multiword search | Help documentation

--- cogs/search.py
+++ cogs/search.py
@@ -4,6 +4,7 @@ from typing import List
 
 from .utils.paginator import Paginator
 
+
 class Search(commands.Cog):
 	'''Search the manga.'''
 
@@ -16,11 +17,37 @@ class Search(commands.Cog):
 		async with self.bot.session.post(page, data= data) as resp:
 			return await resp.json()
 
+	def pick_common_entries(self, data : dict) -> dict:
+		dicts= [list(val.values())[0] for val in data.values()]	
+		keys = set.intersection(*tuple(set(d.keys()) for d in dicts))
+
+		results = {}
+		for key in keys:
+			l = []
+			for d in dicts:
+				l.append(d[key])
+
+			c = self.onlycommon(l)
+			if c is not None:
+				results[key] = c
+
+		return results
+
+	def onlycommon(self, lists: list) -> list:
+		if len(lists) == 1:
+			return lists[0]
+		s = list(set(lists[0]).intersection(*lists))
+		
+		if s:
+			return s
+		return None
+
+
 	def format_to_link(self, chapter: str, page : int) -> str:
 		return f'https://guya.moe/{chapter}/{page}'
 	
 	def format_to_line(self, chapter : str, pages : List[int] ) -> str:
-		string = f'`Ch. {chapter}` | Pages: '
+		string = f'`Ch. {chapter.replace("-" , ".")}` | Pages: '
 		string+= ', '.join([f'[{page}](https://guya.moe/{chapter}/{page})' for page in pages])
 		return string
 
@@ -38,30 +65,48 @@ class Search(commands.Cog):
 
 		return groups
 
-	@commands.command(name='search')
+	@commands.command(name='search', aliases= ['searchtext'])
 	async def _search(self, ctx: commands.Context, *, text: str):
-		f'''
+		'''
 		Search the whole manga for a certain piece of text!
 		This is just `guya.moe`'s search api, implemented and formatted for discord.
 
-		Examples:
-		`{ctx.clean_prefix}search shinomiya`
+		Please note that this search is extremely inaccurate in certain cases.
+		The api actually provides invidiual search results for each word if your query is multi-worded.  The final results are provided by me crossmatching those results in a number of ways.
+		Due to this and how the api provides the results, there's no way to tell if your query (if multi-worded) is literally in the page or the words of the query are in different parts of the page text. 
+		For example, searching "hey there" will assume that "hey, go there" is a valid result.
+		Along with that, there might be some bugs with my crossmatching, producing invalid results.
+
+
+		**Examples:**
+		`search shinomiya`
+		`search never gonna give you up`
 		'''
-		
-		await ctx.trigger_typing() # Tends to take
-		result = (await self.search_in_manga(text))[text]
+
+		await ctx.trigger_typing() # Tends to take some time.
+		result = (await self.search_in_manga(text))
+
 		if not result:
 			return await ctx.send('Nothing found :(')
 		
-		data = list(result.values())[0] # We'll only consider the first result.
+		try:
+			if len(text.split()) > 1:
+				data= self.pick_common_entries(result)
+			else:
+				data= list(result[text].values())[0] # We'll only consider the first result.
+		except IndexError:
+			return await ctx.send('Ran into an error, sorry.')
+
+		if not data:
+			return await ctx.send('Nothing found :(')
 
 		chapters = [int(x) if x.is_integer() else x 
 					for x in sorted([float(i.replace('-','.')) 
 					for i in data])]
-		
+
 		formatted_chapters = [str(c).replace('.','-') for c in chapters]
 
-		lines= [self.format_to_line(chapters[c], data[formatted_chapters[c]])
+		lines= [self.format_to_line(formatted_chapters[c], data[formatted_chapters[c]])
 				for c in range(len(formatted_chapters))]
 
 		groups = self.prepare_for_paginator(lines)
